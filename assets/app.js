9de26491527cdec9446beb761b9cb2bdebb6b062
sekou78 <s.cisse78@gmail.com>
2024-07-20 19:50:42 +0200

    etape 2 du live mini site repos

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,3 @@
-import { Octokit } from "https://esm.sh/octokit";
+import { Home } from "./modules/Home.js";
 
-const octokit = new Octokit({});
-// console.log(octokit);
-
-//octokit.rest.repos => provient du SDK
-//get => Méthode du SDK pour récupérer ici les repos
-octokit.rest.repos
-  .get({
-    owner: "sekou78",
-    repo: "Restaurant",
-  })
-  .then(({ data }) => {
-    console.log("Repo récupérer", data);
-    console.log(data.url);
-  });
+new Home();
