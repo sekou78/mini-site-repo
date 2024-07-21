a8ec78d19437840d92942debe2c6b20f3e174e55
sekou78 <s.cisse78@gmail.com>
2024-07-21 15:48:17 +0200

    etape 4 du mini site repos

--- assets/modules/Home.js
+++ assets/modules/Home.js
@@ -35,14 +35,36 @@ class Home {
   }
 
   async getReposInformations() {
-    const octokit = new Octokit();
+    //API exemple #2 : Récuperer le contenu avec l'Octokit JS et avec "async / await"
     //URL de l'API classique : https://api.github.com/users/sekou78/repos
+    const octokit = new Octokit();
     const response = await octokit
       .request("GET /users/sekou78/repos")
       .catch((error) => {
         console.log("ERREUR lors de l'appel api getReposInformation", error);
       });
-    this.updateHTMLProjects(response.data);
+
+    const recentsProjects = response.data.slice(-3);
+    //URL pour récuperer les langages d'un projet:
+    //https://api.github.com/repos/sekou78/{nom-du-repos}/languages
+    for (let i = 0; i < recentsProjects.length; i++) {
+      const languagesUrl = recentsProjects[i].languages_url;
+      const cleanedUrl = languagesUrl.replace("https://api.github.com", "");
+      const responseLanguages = await octokit
+        .request(`GET ${cleanedUrl}`)
+        .catch((error) => {
+          console.log(
+            "ERREUR lors de l'appel api getReposInformation-langages",
+            error
+          );
+        });
+
+      const projectLanguages = responseLanguages.data;
+      // console.log(projectLanguages);
+      recentsProjects[i].languages = projectLanguages;
+      // console.log(recentsProjects[i].languages);
+    }
+    this.updateHTMLProjects(recentsProjects);
   }
 
   updateHTMLUser(APIdata) {
@@ -61,10 +83,30 @@ class Home {
       const project = projects[i];
       this.projectsTitle[htmlIndex].textContent = project.name;
       this.projectsDescription[htmlIndex].textContent = project.description;
-      const topics = project.topics;
-      console.log(topics);
+      // const languages = project.languages;
+      // console.log(languages);
+
+      this.createHTMLLanguageTag(
+        this.projectsTagsContainer[i],
+        project.languages
+      );
       htmlIndex++;
     }
+  }
+
+  createHTMLLanguageTag(div, languages) {
+    //où ? Je crée mes éléments HTML
+    //quels éléments ? les langages
+    console.log("div", div);
+    console.log("languages", languages);
+    const arrayLanguages = Object.keys(languages);
+    // console.log("array", arrayLanguages);
+    for (let i = 0; i < arrayLanguages.length; i++) {
+      const span = document.createElement("span");
+      span.textContent = arrayLanguages[i];
+      div.appendChild(span);
+    }
+    console.log("array", arrayLanguages);
   }
 }
 
